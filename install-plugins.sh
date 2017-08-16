3ae746bad3008bca530daf32b5d978ebf2b9f701
Ching Yi, Chan <chingyichan.tw@gmail.com>
2017-08-16 11:48:46 +0800

    update custom log format

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,7 +1,7 @@
 sudo td-agent-gem install fluent-plugin-secure-forward
 sudo td-agent-gem install fluent-plugin-record-modifier
 sudo td-agent-gem install fluent-plugin-ua-parser
-# sudo td-agent-gem install fluent-plugin-elasticsearch
+sudo td-agent-gem install fluent-plugin-elasticsearch
 
 
 
